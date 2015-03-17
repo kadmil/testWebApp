7bb3bc000abf99a30671c9ba218a65dba6889ada
Victor Suzdalev <victor.suzdalev@gmail.com>
2015-03-17 11:02:35 +0300

    fix for mobile

--- script.js
+++ script.js
@@ -1,61 +1,134 @@
-particlesJS('particles-js', {
-  particles: {
-    color: '#fff',
-    color_random: false,
-    shape: 'circle',
-    opacity: {
-      opacity: 0.7,
-      anim: {
-        enable: false,
-        speed: 1.5,
-        opacity_min: 0,
-        sync: false
-      }
-    },
-    size: 4.5,
-    size_random: true,
-    nb: 100,
-    line_linked: {
-      enable_auto: true,
-      distance: 140,
+/* DESKTOP */
+
+function pJS_desktop(){
+  particlesJS('particles-js', {
+    particles: {
       color: '#fff',
-      opacity: 1,
-      width: 1,
-      condensed_mode: {
-        enable: false,
-        rotateX: 600,
-        rotateY: 600
-      }
-    },
-    anim: {
-      enable: true,
-      speed: 1
-    }
-  },
-  interactivity: {
-    enable: true,
-    mouse: {
-      distance: 250
-    },
-    detect_on: 'canvas', // "canvas" or "window"
-    mode: 'grab', // "grab" of false
-    line_linked: {
-      opacity: .5
-    },
-    events: {
-      onclick: {
-        enable: true,
-        mode: 'push', // "push" or "remove"
-        nb: 4
+      shape: 'circle',
+      opacity: {
+        opacity: 1
       },
-      onresize: {
+      size: 4.5,
+      size_random: true,
+      nb: 150,
+      line_linked: {
+        enable_auto: true,
+        distance: 250,
+        color: '#fff',
+        opacity: 0.5,
+        width: 1,
+        condensed_mode: {
+            enable: false,
+            rotateX: 600,
+            rotateY: 600
+        }
+      },
+      anim: {
         enable: true,
-        mode: 'out', // "out" or "bounce"
-        density_auto: false,
-        density_area: 800 // nb_particles = particles.nb * (canvas width *  canvas height / 1000) / density_area
+        speed: 3
       }
+    },
+    interactivity: {
+      enable: true,
+      mouse: {
+        distance: 200
+      },
+      detect_on: 'canvas',
+      mode: 'grab',
+      line_linked: {
+        opacity: .5
+      },
+      events: {
+        onclick: {
+          enable: true,
+          mode: 'push',
+          nb: 4
+        }
+      }
+    },
+    retina_detect: true
+  });
+}
+
+
+
+/* MOBILE / TABLET */
+
+function pJS_mobile(){
+  particlesJS('particles-js', {
+    particles: {
+      color: '#fff',
+      shape: 'circle',
+      opacity: 1,
+      size: 2.5,
+      size_random: true,
+      nb: 40,
+      line_linked: {
+        enable_auto: false,
+        distance: 250,
+        color: '#fff',
+        opacity: 0.5,
+        width: 1,
+        condensed_mode: {
+            enable: false,
+            rotateX: 600,
+            rotateY: 600
+        }
+      },
+      anim: {
+        enable: true,
+        speed: 3
+      }
+    },
+    interactivity: {
+      enable: false,
+      mouse: {
+        distance: 200
+      },
+      detect_on: 'canvas',
+      mode: 'grab',
+      line_linked: {
+        opacity: .5
+      },
+      events: {
+        onclick: {
+          enable: true,
+          mode: 'push',
+          nb: 4
+        }
+      }
+    },
+    retina_detect: true
+  });
+}
+
+
+/* LAUNCH */
+
+if(window.innerWidth > 1100){
+  pJS_desktop();
+}else{
+  pJS_mobile();
+}
+
+/* on resize */
+
+window.addEventListener('resize', function() {
+  checkOnResize();
+}, true);
+
+function checkOnResize(){
+  if(window.innerWidth > 1100){
+    if(pJS.particles.nb != 150){
+      console.log('desktop mode')
+      pJS.fn.vendors.destroy();
+      pJS_desktop();
     }
-  },
-  /* Retina Display Support */
-  retina_detect: true
-});
+  }else{
+    if(pJS.particles.nb == 150){
+      console.log('mobile mode');
+      pJS.fn.vendors.destroy();
+      pJS_mobile();
+    }
+  }
+}
