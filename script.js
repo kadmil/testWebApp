a749004908f12225c72710d039fabe4b3bd277e1
Victor Suzdalev <victor.suzdalev@gmail.com>
2015-03-17 11:15:26 +0300

    Is it any better on mobile?

--- script.js
+++ script.js
@@ -1,134 +1,130 @@
 /* DESKTOP */
 
-function pJS_desktop(){
-  particlesJS('particles-js', {
-    particles: {
-      color: '#FFC107',
-      shape: 'circle',
-      opacity: {
-        opacity: 1
-      },
-      size: 4.5,
-      size_random: true,
-      nb: 150,
-      line_linked: {
-        enable_auto: true,
-        distance: 250,
-        color: '#FFC107',
-        opacity: 0.5,
-        width: 1,
-        condensed_mode: {
-            enable: false,
-            rotateX: 600,
-            rotateY: 600
-        }
-      },
-      anim: {
-        enable: true,
-        speed: 3
-      }
-    },
-    interactivity: {
-      enable: true,
-      mouse: {
-        distance: 200
-      },
-      detect_on: 'canvas',
-      mode: 'grab',
-      line_linked: {
-        opacity: .5
-      },
-      events: {
-        onclick: {
-          enable: true,
-          mode: 'push',
-          nb: 4
-        }
-      }
-    },
-    retina_detect: true
-  });
+function pJS_desktop() {
+    particlesJS('particles-js', {
+        particles: {
+            color: '#FFC107',
+            shape: 'circle',
+            opacity: {
+                opacity: 1
+            },
+            size: 4.5,
+            size_random: true,
+            nb: 150,
+            line_linked: {
+                enable_auto: true,
+                distance: 250,
+                color: '#FFC107',
+                opacity: 0.5,
+                width: 1,
+                condensed_mode: {
+                    enable: false,
+                    rotateX: 600,
+                    rotateY: 600
+                }
+            },
+            anim: {
+                enable: true,
+                speed: 3
+            }
+        },
+        interactivity: {
+            enable: true,
+            mouse: {
+                distance: 200
+            },
+            detect_on: 'canvas',
+            mode: 'grab',
+            line_linked: {
+                opacity: .5
+            },
+            events: {
+                onclick: {
+                    enable: true,
+                    mode: 'push',
+                    nb: 4
+                }
+            }
+        },
+        retina_detect: true
+    });
 }
 
 
 
 /* MOBILE / TABLET */
 
-function pJS_mobile(){
-  particlesJS('particles-js', {
-    particles: {
-      color: '#FFC107',
-      shape: 'circle',
-      opacity: 1,
-      size: 2.5,
-      size_random: true,
-      nb: 40,
-      line_linked: {
-        enable_auto: false,
-        distance: 250,
-        color: '#FFC107',
-        opacity: 0.5,
-        width: 1,
-        condensed_mode: {
+function pJS_mobile() {
+    particlesJS('particles-js', {
+        particles: {
+            color: '#FFC107',
+            shape: 'circle',
+            opacity: 1,
+            size: 2.5,
+            size_random: true,
+            nb: 150,
+            line_linked: {
+                enable_auto: false,
+                distance: 250,
+                color: '#FFC107',
+                opacity: 0.5,
+                width: 1,
+                condensed_mode: {
+                    enable: false,
+                    rotateX: 600,
+                    rotateY: 600
+                }
+            },
+            anim: {
+                enable: true,
+                speed: 3
+            }
+        },
+        interactivity: {
             enable: false,
-            rotateX: 600,
-            rotateY: 600
-        }
-      },
-      anim: {
-        enable: true,
-        speed: 3
-      }
-    },
-    interactivity: {
-      enable: false,
-      mouse: {
-        distance: 200
-      },
-      detect_on: 'canvas',
-      mode: 'grab',
-      line_linked: {
-        opacity: .5
-      },
-      events: {
-        onclick: {
-          enable: true,
-          mode: 'push',
-          nb: 4
-        }
-      }
-    },
-    retina_detect: true
-  });
+            mouse: {
+                distance: 200
+            },
+            detect_on: 'canvas',
+            mode: 'grab',
+            line_linked: {
+                opacity: .5
+            },
+            events: {
+                onclick: {
+                    enable: true,
+                    mode: 'push',
+                    nb: 4
+                }
+            }
+        },
+        retina_detect: true
+    });
 }
 
 
 /* LAUNCH */
 
-if(window.innerWidth > 320){
-  pJS_desktop();
-}else{
-  pJS_mobile();
+if (window.innerWidth > 320) {
+    pJS_desktop();
+} else {
+    pJS_mobile();
 }
 
 /* on resize */
 
 window.addEventListener('resize', function() {
-  checkOnResize();
+    checkOnResize();
 }, true);
 
-function checkOnResize(){
-  if(window.innerWidth > 320){
-    if(pJS.particles.nb != 150){
-      console.log('desktop mode')
-      pJS.fn.vendors.destroy();
-      pJS_desktop();
+function checkOnResize() {
+    if (window.innerWidth > 320) {
+        console.log('desktop mode')
+        pJS.fn.vendors.destroy();
+        pJS_desktop();
+    } else {
+        console.log('mobile mode');
+        pJS.fn.vendors.destroy();
+        pJS_mobile();
     }
-  }else{
-    if(pJS.particles.nb == 150){
-      console.log('mobile mode');
-      pJS.fn.vendors.destroy();
-      pJS_mobile();
-    }
-  }
 }
